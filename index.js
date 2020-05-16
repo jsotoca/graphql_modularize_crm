b6ac9c528b6b2949b615c34a4a46381460955140
jsotoca <jsotoca@unprg.edu.pe>
2020-05-15 20:07:24 -0500

    configuracion modificada del puerto

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./src/server');
-const {MONGODB_URI,APPLICACTION_NAME} = require('./src/config');
+const {PORT,MONGODB_URI,APPLICACTION_NAME} = require('./src/config');
 const mongoose = require('mongoose');
 
 mongoose.connect(
@@ -7,7 +7,7 @@ mongoose.connect(
     {useCreateIndex:true,useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false},
     (err)=>{
         if(err)throw err;
-        server.listen().then(({url})=>{
+        server.listen({port:PORT}).then(({url})=>{
             console.log(`${APPLICACTION_NAME} is running in ${url}`);
         });
     }
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV!=='production') require('dotenv').config();
 
 module.exports ={
+    PORT:process.env.PORT,
     MONGODB_URI:process.env.MONGODB_URI,
     TOKEN_SECRET:process.env.TOKEN_SECRET,
     APPLICACTION_NAME:process.env.APPLICACTION_NAME
